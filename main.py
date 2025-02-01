232c4d35b9a723294206d1c54ee52b75277ec1f3
Speed2Quick <4samuelr05@gmail.com>
2025-01-31 19:21:08 -0500

    add character counter

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 def main():    
     text = read_contents()
     words = word_count(text)
-    print(words)
+    characters = character_counter(text)
+    print(characters)
+
+def read_contents():
+    with open("books/frankenstein.txt") as f:
+        return f.read()
 
 def word_count(book):
     split_str = book.split()
     return len(split_str)
 
+def character_counter(book):
+    lower_case_words = book.lower()
+    num_characters = {}
+    character_list = list("abcdefghijklmnopqrstuvwyxz0123456789!@#$%^&* ")
+    for letter in character_list:
+        counter = lower_case_words.count(letter)
+        num_characters.update({letter : counter})
+    return num_characters
 
-def read_contents():
-    with open("books/frankenstein.txt") as f:
-        return f.read()
+
+
+
 
 
 
