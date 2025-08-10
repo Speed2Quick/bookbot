cf2b8a4805405472521d712ea49812bee81bc503
Speed2Quick <4samuelr05@gmail.com>
2025-08-09 23:12:50 -0400

    add report for book stats

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from stats import word_count, character_count
+from stats import word_count, character_count, character_sort
+import sys
 
 def main():
 
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+    text = get_book_text(book_path)
     characters = character_count(text)
-    print(f"{word_count(text)} words found in the document")
-    print(character_count(text))
+    sorted_characters = character_sort(characters)
+
+    print("========== BOOKBOT ==========")
+    print(f"Analyzing book found at {book_path}...")
+    print("--------- Word Count ---------")
+    print(f"Found {word_count(text)} total words")
+    print("-------- Character Count -----")
+    for dict in sorted_characters:
+        if dict["char"].isalpha():
+            print(f"{dict["char"]}: {dict["num"]}")
+    print("========== END ==========")
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -10,3 +10,14 @@ def character_count(text):
         else:
             characters[letter] += 1
     return characters
+
+def sort_on(item):
+    return item["num"]
+
+def character_sort(dict):
+    sorted_dicts = []
+    for character in dict:
+        new_dict = {"char": character, "num": dict[character]}
+        sorted_dicts.append(new_dict)
+    sorted_dicts.sort(reverse=True, key=sort_on)
+    return sorted_dicts
